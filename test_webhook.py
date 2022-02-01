8b78e67d6edca67a0ad0fe266b410857cba3d3eb
fanny-umana <fanny.umana@netbraintech.com>
2022-02-01 15:48:48 +0100

    test webhook

---
+++ test_webhook.py
@@ -0,0 +1,27 @@
+# Import Module
+import os
+import fnmatch
+  
+# Folder Path
+path = "raw_data"
+  
+# Change the directory
+os.chdir(path)
+  
+# Read text File
+  
+  
+def read_text_file(file_path):
+    with open(file_path, 'r') as f:
+        print(f.read())
+  
+  
+# iterate through all file
+for file in os.listdir():
+    # Check whether file is in text format or not
+    if fnmatch.fnmatch(file, '*raw*'):
+        # file_path = f"{path}\{file}"
+  
+        # call read text file function
+        print("ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ")
+        read_text_file(file)
