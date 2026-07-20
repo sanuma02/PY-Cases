846e6a3753d39c1e62ec39a2f649e174b2a9b101
Fanny Sanchez <fanitasanuma20@hotmail.com>
2026-07-20 07:35:55 -0600

    Add sample.py with variable manipulation and list comprehension

---
+++ sample.py
@@ -0,0 +1,9 @@
+squares = [x*2 for x in range(5)]
+
+##########################################
+the_var = "Netbrain"
+if the_var[3].lower():
+	the_var[3] = "B"
+print(the_var)
+
+##############################################
